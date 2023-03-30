5ec26d080178feb93b51b40874137d9ed5307eca
hyegyeong-kang <rkdgprud96@gmail.com>
2023-03-30 16:07:16 +0900

    update files

--- spring-docker/spring-docker/src/main/java/com/example/springdocker/controller/HelloController.java
+++ spring-docker/spring-docker/src/main/java/com/example/springdocker/controller/HelloController.java
@@ -1,13 +1,35 @@
 package com.example.springdocker.controller;
 
+import com.example.springdocker.service.MemberService;
+import com.example.springdocker.vo.MemberVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.SecureRandom;
+import java.util.List;
 
 @RestController
 public class HelloController {
+    @Autowired
+    MemberService memberService;
+
     @GetMapping("/")
     public String hello() {
         return "Hello Docker222";
+    }
+
+    @GetMapping("/members")
+    public List<MemberVO> selectListMembers() {
+        return memberService.selectAllMembers();
+    }
+
+    @PostMapping("/addMember")
+    public String addMember(@RequestBody MemberVO memberVO) {
+        memberService.insertMember(memberVO);
+        return "OK";
     }
 
 }
